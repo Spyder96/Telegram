0298303b7af58cb6af76dc7e3f3b057fefb9e880
Spyder96 <Amirul.1216@gmail.com>
2023-04-26 03:36:50 +0530

    basic yfinance intergrated to Bot

--- Bot.py
+++ Bot.py
@@ -1,36 +1,61 @@
-
 import telegram
 from dotenv import load_dotenv
 import os
+import YFinance as YF
 
 load_dotenv()
 TelegramBotCredential = os.environ.get('BOT_TOKEN')
 
 import logging
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
+
 def start(update, context):
-    update.message.reply_text ("Hi! I'm a bot. How can I help you?")
-    
+    update.message.reply_text("Hi! I'm a bot. How can I help you?")
+
+
+def handle_message(update, context):
+    text = str(update.message.text).lower()
+
+
 def search(update, context):
-    update.message.reply_text ("Enter text to search?")
     ticker = context.args[0]
-    
-    
+
+    stock = YF.Stock(ticker)
+    update.message.reply_text(f" Getting Stock Details of :  {ticker} . Please wait... ")
+    stock.get_data()
+    if stock.data is None:
+        update.message.reply_text(f" Failed to get Data for {ticker} ")
+        logging.info(f"Failed to get stock data ")
+    else:
+        update.message.reply_text(f"last details : \n{stock.show_data()} ")
+    # except Exception as e:
+    #    logging.info(f"Failed to get stock data: {e} ")
+
+
+#
+
+
+# dispatcher.add_handler(MessageHandler( Filters.text, handle_message ))
+#  with open('plot.png', 'rb') as f:
+#     context.bot.send_photo(chat_id=update.effective_chat.id, photo=f)
+
+
 def main():
     updater = Updater(TelegramBotCredential)
     dispatcher = updater.dispatcher
-    
+
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("search", search))
-    
+
     updater.start_polling()
     updater.idle()
-    
+
+
 if __name__ == '__main__':
     main()
--- YFinance.py
+++ YFinance.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import ta
 import yfinance as yf
 import pandas as pd
@@ -14,101 +11,92 @@ from scipy.stats import linregress
 
 
 class Stock:
-    
-    def __init__(self):
-        self.name = ''
 
+    def __init__(self, name='', interval=365):
+        self.ticker = name
+        self.data = None
+        self.start_date = (datetime.datetime.today() -
+                           datetime.timedelta(days=interval)).strftime('%Y-%m-%d')
+        self.end_date = datetime.datetime.today().strftime('%Y-%m-%d')
+        self.df = None
+        self.short_term_period = None
+        self.mid_term_period = None
+        self.long_term_period = None
 
-    def stock(self):
-        
-        #today's date
-        end_date = datetime.datetime.today().strftime('%Y-%m-%d')
-        
-        # Calculate the start date as one year ago from today
-        start_date = (datetime.datetime.today() - datetime.timedelta(days=365)).strftime('%Y-%m-%d')
-        
-        
-        #data = yf.download(self.name, start=start_date, end=end_date)
-        
-        # Download data for TCS for the last year
-        #data = yf.download('TCS.NS', start=start_date, end=end_date)
-    
-        # caluculating EMA's
-        
-        # data['EMA9'] = data['Close'].ewm(span=9, adjust=False).mean()
-        # data['EMA20'] = data['Close'].ewm(span=20, adjust=False).mean()
-        # data['EMA50'] = data['Close'].ewm(span=50, adjust=False).mean()
-        # data['EMA100'] = data['Close'].ewm(span=100, adjust=False).mean()
-        # data['EMA200'] = data['Close'].ewm(span=200, adjust=False).mean()
-        
-        # # Compute the 20 day average volume
-        # data['VolumeAvg20'] = data['Volume'].rolling(window=20).mean()
-        
-        
-        # data['RSI'] = ta.momentum.RSIIndicator(data['Close'], window=14).rsi()
-        
-        # macd = ta.trend.MACD(data['Close'], window_fast=12, window_slow=26, window_sign=9)
-        # data['MACD'] = macd.macd()
-        # data['MACD_signal'] = macd.macd_signal()
-        # data['MACD Above Signal'] = data['MACD'] > data['MACD_signal']
-        
-        
-        
-        
+    def get_data(self):
+        self.data = yf.download(self.ticker, start=self.start_date, end=self.end_date)
+        self.df = pd.DataFrame(self.data)
 
-        def calculate_technical_indicators(name, start_date, end_date, ema_span=9, ema_span_list=[20, 50, 100, 200], rsi_window=14, macd_fast=12, macd_slow=26, macd_signal=9):
-            data = yf.download(name, start=start_date, end=end_date)
-            data['EMA{}'.format(ema_span)] = data['Close'].ewm(span=ema_span, adjust=False).mean()
-            for span in ema_span_list:
-                data['EMA{}'.format(span)] = data['Close'].ewm(span=span, adjust=False).mean()
-            data['VolumeAvg20'] = data['Volume'].rolling(window=20).mean()
-            data['RSI'] = ta.momentum.RSIIndicator(data['Close'], window=rsi_window).rsi()
-            macd = ta.trend.MACD(data['Close'], window_fast=macd_fast, window_slow=macd_slow, window_sign=macd_signal)
-            data['MACD'] = macd.macd()
-            data['MACD_signal'] = macd.macd_signal()
-            data['MACD Above Signal'] = data['MACD'] > data['MACD_signal']
-            return data
-        data = calculate_technical_indicators(self.name, start_date, end_date)
-        df = pd.DataFrame(data)
-        
-    
- 
+    def show_data(self, period=1):
+        return self.df.tail(period).T.apply(lambda x: round(x, 2))
+
+    def calculate_technical_indicators(stock, ema_span=9, ema_span_list=None, rsi_window=14, macd_fast=12,
+                                       macd_slow=26, macd_signal=9):
+        if ema_span_list is None:
+            ema_span_list = [20, 50, 100, 200]
+        stock.df['EMA{}'.format(ema_span)] = stock.df['Close'].ewm(span=ema_span, adjust=False).mean()
+        for span in ema_span_list:
+            stock.df['EMA{}'.format(span)] = stock.df['Close'].ewm(span=span, adjust=False).mean()
+        stock.df['VolumeAvg20'] = stock.df['Volume'].rolling(window=20).mean()
+        stock.df['RSI'] = ta.momentum.RSIIndicator(stock.df['Close'], window=rsi_window).rsi()
+        macd = ta.trend.MACD(stock.df['Close'], window_fast=macd_fast, window_slow=macd_slow, window_sign=macd_signal)
+        stock.df['MACD'] = macd.macd()
+        stock.df['MACD_signal'] = macd.macd_signal()
+        stock.df['MACD Above Signal'] = stock.df['MACD'] > stock.df['MACD_signal']
+
     # Define a function to calculate the slope and y-intercept of a trendline
-        def calculate_trendline(x, y):
+
+    def set_period_values(self, st=10, mt=50, lt=100):
+        self.short_term_period = st
+        self.mid_term_period = mt
+        self.long_term_period = lt
+
+    def trendline(self):
+        def trend_data(x, y):
             slope, intercept, r_value, p_value, std_err = linregress(x, y)
             return slope * x + intercept
-    
-    # Define the time periods for each trendline
-        short_term_period = 10
-        mid_term_period = 50
-        long_term_period = 100
-        
-        # Calculate the short-term trendline using linear regression
-        short_term_trendline = calculate_trendline(np.arange(len(df['Close']))[-short_term_period:], df['Close'][-short_term_period:])
-        
-        # Calculate the mid-term trendline using linear regression
-        mid_term_trendline = calculate_trendline(np.arange(len(df['Close']))[-mid_term_period:], df['Close'][-mid_term_period:])
-        
-        # Calculate the long-term trendline using linear regression
-        long_term_trendline = calculate_trendline(np.arange(len(df['Close']))[-long_term_period:], df['Close'][-long_term_period:])
-        
-        # Plot the price data and trendlines
+
+        def calculate_trendline(df, period):
+            return trend_data(np.arange(len(df['Close']))[-period:], df['Close'][-period:])
+
+        self.short_term_trendline = calculate_trendline(self.df, self.short_term_period)
+        self.mid_term_trendline = calculate_trendline(self.df, self.mid_term_period)
+        self.long_term_trendline = calculate_trendline(self.df, self.long_term_period)
+
+    # Calculate the short-term trendline using linear regression
+
+    # # Calculate the mid-term trendline using linear regression
+    # mid_term_trendline = calculate_trendline(np.arange(len(df['Close']))[-mid_term_period:], df['Close'][-mid_term_period:])
+
+    # # Calculate the long-term trendline using linear regression
+    # long_term_trendline = calculate_trendline(np.arange(len(df['Close']))[-long_term_period:], df['Close'][-long_term_period:])
+
+    # # Plot the price data and trendlines
+    def plot_trends(self):
+        # if self.short_term_trendline == None:
+        #     trendline()
+
         fig, ax = plt.subplots(figsize=(10, 6))
-        
-        df['Close'].plot(ax=ax, label='Close')
-        ax.plot(df.index[-short_term_period:], short_term_trendline, label='Short-term trendline')
-        ax.plot(df.index[-mid_term_period:], mid_term_trendline, label='Mid-term trendline')
-        ax.plot(df.index[-long_term_period:], long_term_trendline, label='Long-term trendline')
-        
+
+        self.df['Close'].plot(ax=ax, label='Close')
+        ax.plot(self.df.index[-self.short_term_period:], self.short_term_trendline, label='Short-term trendline')
+        ax.plot(self.df.index[-self.mid_term_period:], self.mid_term_trendline, label='Mid-term trendline')
+        ax.plot(self.df.index[-self.long_term_period:], self.long_term_trendline, label='Long-term trendline')
+
         ax.legend()
         plt.savefig('plot.png', dpi=300, bbox_inches='tight')
-        plt.show()
-    
+        # plt.show()
+
+
 def main():
-    stock = Stock()
-    stock.name = 'TCS.NS'
-    stock.stock()
-    
-    
+    tcs = Stock('TCS.NS')
+
+    tcs.get_data()
+    tcs.show_data()
+#    tcs.set_period_values()
+#    tcs.trendline()
+#    tcs.plot_trends()
+
+
 if __name__ == '__main__':
     main()
